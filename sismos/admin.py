18a2358ee50ca7b05e5e17a4a1d6d3494242a2da
94leonardo <lassoleonardo56@gmail.com>
2025-06-20 17:43:12 -0500

    creacion de modelos feature

--- sismos/admin.py
+++ sismos/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import  Feature, Comment
 
 # Register your models here.
+
+
+admin.site.register(Feature)
+admin.site.register(Comment)
+
+
+
+
--- sismos/models.py
+++ sismos/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Feature(models.Models):
+    external_id = models.CharField(max_length=100, unique=True)
+    magnitude = models.DecimalField(max_digits=4, decimal_places=1)
+    place = models.CharField(max_length=255)
+    time = models.DateField()
+    tsunami = models.BooleanField()
+    mag_type = models.CharField(max_length=10)
+    title = models.TextField()
+    longitude = models.DecimalField(max_digits=9,decimal_places=6)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    url = models.URLField()
+
+    def __str__(self):
+        return f"{self.title} ({self.magnitude})"
+
+class Comment(models.Model):
+    Feature = models.ForeignKey(Feature, related_name='comments', on_delete=models.CASCADE)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment on {self.Feature.id} : {self.body[:30]}..."
+    
